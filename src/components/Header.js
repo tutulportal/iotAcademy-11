d69bd078d0ec80fc8052e48f612a33fc2443a2ad
tutulportal <tutulvpn3@gmail.com>
2022-10-26 08:44:25 +0600

    menu design complete without responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import logo from '../logo.png';
 import { Link } from 'react-router-dom';
+import './MasterComponentsCss.css';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react';
 
 const Header = () => {
-    return (
-        <div>
-            <div className="logo">
-                <img src={logo} alt={logo} />
-            </div>
-            <nav className='menu'>
-                <li><Link to={'/home'}>Home</Link></li>
-                <li><Link to={'/courses'}>Courses</Link></li>
-                <li><Link to={'/faq'}>FAQ</Link></li>
-                <li><Link to={'/blog'}>Blog</Link></li>
-            </nav>
-            <div className="side-extra-menu">
+    const [darkMode, setDarkMode] = useState(false);
 
+    const handleDarkMode = () => setDarkMode(!darkMode);
+
+    return (
+        <div className='header-bar'>
+            <div className="inner-header-bar">
+                <div className="logo">
+                    <Link to='/'><img src={logo} alt={logo} /></Link>
+                </div>
+                <nav className='menu'>
+                    <li><Link to={'/home'}>Home</Link></li>
+                    <li><Link to={'/courses'}>Courses</Link></li>
+                    <li><Link to={'/faq'}>FAQ</Link></li>
+                    <li><Link to={'/blog'}>Blog</Link></li>
+                </nav>
+                <div className="side-extra-menu">
+                    <span className='modes'>
+                        {
+                            darkMode ? <SunIcon onClick={handleDarkMode} title="Click to make Light Mode" height="24px"/> : <MoonIcon onClick={handleDarkMode} title="Click to make Dark Mode" height="24px"/>
+                        }
+                    </span>
+                </div>
             </div>
         </div>
     );
