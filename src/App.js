97395d4a66be6b4c0f41d426377a4f5cd4d9e1f4
tutulportal <tutulvpn3@gmail.com>
2022-10-26 07:28:30 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useContext } from 'react';
 import './App.css';
+import Main from './layouts/Main';
+import HomePage from './components/HomePage';
+import { AuthContext } from './contexts/auth.context';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const authTest = useContext(AuthContext);
+  console.log(authTest);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+      children: [
+        {
+          path: "/",
+          element: <HomePage/>
+        },
+        {
+          path: "/home",
+          element: <HomePage/>
+        }
+      ]
+    },
+    {
+      path: "*",
+      element: <div>Not found</div>
+    }
+  ])
+
   return (
     <div>
-      
+      <RouterProvider router={router}/>
     </div>
   );
 }
